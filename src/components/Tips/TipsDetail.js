07f2db366e15a234c3a0ba0878d76b0e7070bbe2
numanash <mnuman2772@gmail.com>
2019-07-27 14:27:22 +0500

    WEbview done

--- src/components/Tips/TipsDetail.js
+++ src/components/Tips/TipsDetail.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
 import { withNavigation } from "react-navigation";
+import { WebView } from "react-native-webview";
+
 class TipsDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tip: undefined
-        }
-    }
-    componentDidMount() {
-        const { navigation } = this.props;
-        fetch(`https://www.winsomeglow.com/api/posts/${navigation.getParam('id')}/${navigation.getParam('post_slug')}`)
-            .then(res => res.json())
-            .then(result => {
-                console.log({ result });
-            }).catch(e => {
-                console.log(e);
-            })
-    }
-    render() {
-        return (<View>
-            <Text>welcome to tips details</Text>
-        </View >);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      tip: undefined
+    };
+  }
+  componentDidMount() {
+    const { navigation } = this.props;
+    fetch(
+      `https://www.winsomeglow.com/api/posts/${navigation.getParam(
+        "id"
+      )}/${navigation.getParam("post_slug")}`
+    )
+      .then(res => res.json())
+      .then(result => {
+        console.log({ result });
+        this.setState({
+          tip: result.post
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+  render() {
+    const { tip } = this.state;
+    tip &&
+      console.log({
+        tip: tip.post_content.replace(
+          "../../../",
+          "http://www.winsomeglow.com/"
+        )
+      });
+    return (
+      <View>
+        <Text>welcome to tips details</Text>
+        {tip && (
+          <ScrollView contentContainerStyle={{ width: "100%", height: "60%" }}>
+            <WebView
+              originWhitelist={["*"]}
+              source={{
+                html: tip.post_content.replace(
+                  "../../../",
+                  "http://www.winsomeglow.com/"
+                )
+              }}
+              startInLoadingState={true}
+              automaticallyAdjustContentInsets={false}
+            />
+          </ScrollView>
+        )}
+      </View>
+    );
+  }
 }
 
 export default withNavigation(TipsDetail);
